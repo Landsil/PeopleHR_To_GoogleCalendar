a0b1375c3b5d63c7888a0fd1c7695fb4d811c922
Landsil <landsil@gmail.com>
2018-12-27 15:48:52 +0000

    Update HR_to_cal.py

--- HR_to_cal.py
+++ HR_to_cal.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python3
 
+from __future__ import print_function
+from apiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+from pprint import pprint
 import requests
 import json
-from pprint import pprint
 import csv
+
+# Read file with API key
+with open('peopleHR_APIkey.txt', 'r') as fp:
+    apikey = fp.read().strip()
 
 # Call to PeopleHR API to get data
 url = 'https://api.peoplehr.net/Employee'
-payload={'APIKey':'<< Your API token here, betwenn those ''>>',
+payload = {'APIKey': apikey,
 	'Action':'GetAllEmployeeDetail',
 	'IncludeLeavers':'false'}
 
 # You have to call as json, output is "inbound"
-r = requests.post(url, json=payload)
-results = r.json()['Results']
+inbound = requests.post(url, json=payload)
+results = inbound.json()['Result']
 
 # Data we need
 permitted = {"EmployeeId", "FirstName", "LastName", "StartDate", "DateOfBirth"}
@@ -21,7 +29,8 @@ permitted = {"EmployeeId", "FirstName", "LastName", "StartDate", "DateOfBirth"}
 #From results list take DisplayValue that is listed in permitted
 list_of_people = [{key:x[key]['DisplayValue'] for key in permitted} for x in results]
 
+# Now we are starting with callendar
 
 
-#list_of_people
-#pprint(_)
+# To have a look un-comment next line
+pprint(list_of_people)
