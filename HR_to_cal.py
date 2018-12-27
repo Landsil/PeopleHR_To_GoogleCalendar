dbfcee689751132c61c44c24e3880b801f83b769
Landsil <landsil@gmail.com>
2018-12-27 16:22:35 +0000

    Update HR_to_cal.py

--- HR_to_cal.py
+++ HR_to_cal.py
@@ -9,9 +9,13 @@ import requests
 import json
 import csv
 
-# Read file with API key
+# Calendar API scopes
+SCOPES = 'https://www.googleapis.com/auth/calendar'
+
+
+# Read file with API key while striping possible white spaces, peopleHR_APIkey.txt contains just a key string
 with open('peopleHR_APIkey.txt', 'r') as fp:
-    apikey = fp.read().strip()
+	apikey = fp.read().strip()
 
 # Call to PeopleHR API to get data
 url = 'https://api.peoplehr.net/Employee'
@@ -21,6 +25,8 @@ payload = {'APIKey': apikey,
 
 # You have to call as json, output is "inbound"
 inbound = requests.post(url, json=payload)
+
+# We only care about "Result" part
 results = inbound.json()['Result']
 
 # Data we need
@@ -29,8 +35,30 @@ permitted = {"EmployeeId", "FirstName", "LastName", "StartDate", "DateOfBirth"}
 #From results list take DisplayValue that is listed in permitted
 list_of_people = [{key:x[key]['DisplayValue'] for key in permitted} for x in results]
 
-# Now we are starting with callendar
+# To have a look at content in format un-comment next line
+#pprint(list_of_people)
 
+""" Now we are starting with callendar
+You can hard code ID if you wish, we will open it from a file again.
+group calendar ID looks like "domain.com_iouherfiuhenriufgh@group.calendar.google.com"
+"""
+with open('calendar_ID.txt', 'r') as fp:
+	my_calendar_ID = fp.read().strip()
 
-# To have a look un-comment next line
-pprint(list_of_people)
+def main():
+	# The file token.json stores the user's access and refresh tokens, and is
+	# created automatically when the authorization flow completes for the first
+	# time.
+	store = file.Storage('token.json')
+	creds = store.get()
+	if not creds or creds.invalid:
+		flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+		creds = tools.run_flow(flow, store)
+	service = build('calendar', 'v3', http=creds.authorize(Http()))
+
+	# Call the Calendar API to clear calendar
+	# https://developers.google.com/calendar/v3/reference/calendars/clear
+	service.calendars().clear(calendarId=my_calendar_ID).execute()
+
+if __name__ == '__main__':
+	main()
