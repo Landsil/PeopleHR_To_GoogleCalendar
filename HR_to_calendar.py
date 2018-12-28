e0f52ca1d84722f1ebd043e2907aa6164744407c
Landsil <landsil@gmail.com>
2018-12-28 16:52:26 +0000

    Update HR_to_calendar.py

--- HR_to_calendar.py
+++ HR_to_calendar.py
@@ -32,15 +32,15 @@ inbound = requests.post(url, json=payload)
 results = inbound.json()['Result']
 
 # Data we need
-permitted = {"EmployeeId", "FirstName", "LastName", "StartDate", "DateOfBirth"}
+permitted = {"FirstName", "LastName", "StartDate", "DateOfBirth"} #"EmployeeId"
 
 #From results list take DisplayValue that is listed in permitted
 list_of_people = [{key:x[key]['DisplayValue'] for key in permitted} for x in results]
 
 # Debug check
-pprint(list_of_people)
-print('Does it look fine?')
-input('Press Enter to continue...')
+#pprint(list_of_people)
+#print('Does it look fine?')
+#input('Press Enter to continue...')
 
 # Autentication to google
 # The file token.json stores the user's access and refresh tokens, and is
@@ -86,75 +86,52 @@ def remove_events():
 if __name__ == '__main__':
     remove_events()
 
+# Now we should create events
+events_list = []
+people_with_missing_DateOfBirth = []
 
-//////////////////
-    for person in list_of_people:
-        # Create nesssesery dates, have to strip time
-        # Execption if date is missing.
-        people_with_missing_DateOfBirth = []
+for person in list_of_people:
+    # Create nesssesery dates, have to strip time
+    # Execption if date is missing.
+    try:
+        DoB = datetime.strptime(person['DateOfBirth'], "%Y-%m-%d").date()
+    except ValueError:
+        people_with_missing_DateOfBirth.append('Missing for {} {}'.format(person['FirstName'], person['LastName']))
+        continue
 
-        try:
-            DoB = datetime.strptime(person['DateOfBirth'], "%Y-%m-%d").date()
-        except ValueError:
-            people_with_missing_DateOfBirth.append('Missing data {} {}'.format(person['FirstName'], person['LastName']))
-            continue
+    now = datetime.now()
+    start_date = DoB.replace(year=now.year)
+    age = start_date.year - DoB.year
+    end_date = start_date + timedelta(days=1)
+    # Create an event body
 
-        now = datetime.now()
-        start_date = DoB.replace(year=now.year)
-        age = start_date.year - DoB.year
-        end_date = start_date + timedelta(days=1)
-        # Create an event body
-        event = {
-          'summary': 'It\'s {} {} Birthday, {} is {} year\'s old'.format(person['FirstName'], person['LastName'], person['FirstName'], age),
-          'start': {
-            'date': str(start_date),
-            'timeZone': 'Europe/London',
-          },
-          'end': {
-            'date': str(end_date),
-            'timeZone': 'Europe/London',
-          },
-          'recurrence': [
-            'RRULE:FREQ=YEARLY;COUNT=2'
-          ],
-        }
+    event = {
+      'summary': 'It\'s {} {} Birthday, {} is {} year\'s old'.format(person['FirstName'], person['LastName'], person['FirstName'], age),
+      'start': {
+        'date': str(start_date),
+        'timeZone': 'Europe/London',
+      },
+      'end': {
+        'date': str(end_date),
+        'timeZone': 'Europe/London',
+      },
+      'recurrence': [
+        'RRULE:FREQ=YEARLY;COUNT=2'
+      ],
+    }   
+    events_list.append(event)
 
-"""
-    # Now we create new events from list_of_people (birthdays)
-    for person in list_of_people:
-        # Create nesssesery dates, have to strip time
-        # Execption if date is missing.
-        people_with_missing_DateOfBirth = []
-
-        try:
-            DoB = datetime.strptime(person['DateOfBirth'], "%Y-%m-%d").date()
-        except ValueError:
-            people_with_missing_DateOfBirth.append('Missing data {} {}'.format(person['FirstName'], person['LastName']))
-            continue
-
-        now = datetime.now()
-        start_date = DoB.replace(year=now.year)
-        age = start_date.year - DoB.year
-        end_date = start_date + timedelta(days=1)
-        # Create an event body
-        event = {
-          'summary': 'It\'s {} {} Birthday, {} is {} year\'s old'.format(person['FirstName'], person['LastName'], person['FirstName'], age),
-          'start': {
-            'date': str(start_date),
-            'timeZone': 'Europe/London',
-          },
-          'end': {
-            'date': str(end_date),
-            'timeZone': 'Europe/London',
-          },
-          'recurrence': [
-            'RRULE:FREQ=YEARLY;COUNT=2'
-          ],
-        }
-        # Insert event into calendar
-        event = service.events().insert(calendarId=my_calendar_ID, body=event).execute()
-
-
-"""
-
+# Debug
 #print(people_with_missing_DateOfBirth)
+#print('Those have to be corrected')
+#input('Press Enter to continue...')
+
+# Debug
+#pprint(events_list)
+#print('Those will be created')
+#input('Press Enter to continue...')
+
+# Insert events into calendar
+for event in event_list:
+    time.sleep(0.1)
+    event = service.events().insert(calendarId=my_calendar_ID, body=event).execute()
